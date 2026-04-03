4ce9787d5eaabeac37c43a86ecaaf3c8c5622eec
HaroldPlant <126260822+HaroldPlant@users.noreply.github.com>
2026-04-03 11:33:05 +0100

    Update FinanceSeperateApplicationTests.java

--- finance_service/src/test/java/com/example/financeseperate/FinanceSeperateApplicationTests.java
+++ finance_service/src/test/java/com/example/financeseperate/FinanceSeperateApplicationTests.java
@@ -1,13 +1,1 @@
-package com.example.financeseperate;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FinanceSeperateApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
